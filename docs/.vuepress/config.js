e5ca954259cb08f2f81cfae8fe4e33ac676fa9d0
Kaspars Dambis <hi@kaspars.net>
2021-12-25 22:18:23 +0200

    Add config

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  title: 'Būvē gudri!',
+  description: 'Ar zinātnisku pieeju būvniecībai',
+  head: [
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
+  ],
+  themeConfig: {
+    repo: '',
+    editLinks: false,
+    docsDir: '',
+    editLinkText: '',
+    lastUpdated: false,
+  },
+  plugins: []
+}
